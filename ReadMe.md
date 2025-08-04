19cbace4f5761b832a4ffb81833de6da1c336c81
nevets-steven <nevets322@gmail.com>
2025-08-04 16:54:14 -0400

    completed first 4 phases from readme

--- ReadMe.md
+++ ReadMe.md
@@ -5,9 +5,9 @@ Create a birthdays.json file with sample entries, completed 8/4
 
 Build a simple HTML page to display all birthdays from the JSON, compelted 8/4
 
-Add JavaScript logic to detect birthdays within 3 days, begin 8/4
+Add JavaScript logic to detect birthdays within 3 days, completed 8/4
 
-Show upcoming birthdays separately on the page
+Show upcoming birthdays separately on the page, completed 8/4
 
 Add a button to create Google Calendar reminders manually
 
--- script.js
+++ script.js
@@ -14,16 +14,52 @@ function getJSONData(){
             console.log(data);
             document.getElementById('outputDiv').innerText = JSON.stringify(data, null, 2); //raw data
 
-            const dataContainer = document.getElementById('dataContainer');
+            const friendBirthday = document.getElementById('friendBirthday');
             for (const friend of data){
-                console.log(friend.name);
-                dataContainer.innerHTML +=
+                // console.log(friend.name);
+                // dataContainer.innerHTML +=
 
-                `<ul>
-                    <li>Name: ${friend.name}</li>
-                    <li>Birthday: ${friend.birthday}</li>
-                    <li>Notes: ${friend.notes}</li>
-                 </ul>`
+                // `<ul>
+                //     <li>Name: ${friend.name}</li>
+                //     <li>Birthday: ${friend.birthday}</li>
+                //     <li>Notes: ${friend.notes}</li>
+                //  </ul>`
+                // console.log(typeof(friend.birthday))
+                const birthdayString = friend.birthday;
+
+                const today = new Date();
+                const currentYear = today.getFullYear();
+                const [month, day, year] = birthdayString.split('/');
+
+                const birthDate = new Date(currentYear, month-1, day);
+                
+
+                if (birthDate < today){
+                    birthDate.setFullYear(currentYear + 1);
+                }
+
+                console.log(birthDate);
+
+                const difference = (birthDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24);
+
+                if (difference >= 0 && difference <= 3){
+                    friendBirthday.innerHTML += 
+                    `<ul>
+                        <li>Name: ${friend.name}</li>
+                        <li>Birthday: ${friend.birthday}</li>
+                        <li>Notes: ${friend.notes}</li>
+                    </ul>`;
+                }
+                else{
+                    const otherBirthday = document.getElementById('otherBirthdays');
+
+                    otherBirthday.innerHTML += 
+                    `<ul>
+                        <li>Name: ${friend.name}</li>
+                        <li>Birthday: ${friend.birthday}</li>
+                        <li>Notes: ${friend.notes}</li>
+                    </ul>`;
+                }
             }
              
         })
